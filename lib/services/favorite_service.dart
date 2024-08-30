2d353dca90abd0e1345cd6e85605484f542a3015
jfocmc1bot <julio.fabio@mc1global.com>
2024-08-30 14:13:56 -0300

    Update favorite_service.dart
    remover localmente favoritos de videos que não existem mais no firebase

--- lib/services/favorite_service.dart
+++ lib/services/favorite_service.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FavoriteService {
   static const String _favoriteVideosKey = 'favoriteVideos';
@@ -29,5 +30,21 @@ class FavoriteService {
       favoriteVideos.add(videoId);
     }
     await _saveFavoriteVideosToPref(favoriteVideos);
+  }
+
+  Future<void> clearNonExistentFavorites() async {
+    final favoriteVideos = await _getFavoriteVideosFromPref();
+    final firebaseVideos =
+        await FirebaseFirestore.instance.collection('videos').get();
+
+    final existingVideoIds = firebaseVideos.docs.map((doc) => doc.id).toSet();
+    final nonExistentVideos =
+        favoriteVideos.where((id) => !existingVideoIds.contains(id)).toList();
+
+    if (nonExistentVideos.isNotEmpty) {
+      final updatedFavoriteVideos =
+          favoriteVideos.toSet().difference(nonExistentVideos.toSet()).toList();
+      await _saveFavoriteVideosToPref(updatedFavoriteVideos);
+    }
   }
 }
